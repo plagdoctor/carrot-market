c8be7367987e9b686920feec9d7cca8b0e0c5ba7
jinyoung <danroom85@gmail.com>
2022-01-22 09:52:25 +0900

    tailwind_group and hover test

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,25 +5,35 @@ const Home: NextPage = () => {
     <div className=" bg-slate-400 py-20 px-20 flex flex-col space-y-5 grid gap-10 min-h-screen">
       <div className=" bg-white p-6 rounded-3xl shadow-xl"> 
         <span className="font-semibold text-3xl">Select Item</span>
-        <div className="flex justify-between">
-          <span className=" text-gray-500">Grey Chair</span>
-          <span className=" font-semibold">$19</span>
-        </div>
-        <div className=" flex justify-between">
-          <span className=" text-gray-500">Grey Chair</span>
-          <span className=" font-semibold">$19</span>
-        </div>        
+        <ul>
+          {[1,2,3,4,5].map((i) => (
+            <div key={i} className=" flex justify-between my-2 first:bg-blue-50 last:bg-blue-50 odd:bg-blue-50 even:bg-yellow-100">
+              <span className=" text-gray-500">Grey Chair</span>
+              <span className=" font-semibold">$19</span>
+            </div>
+          )
+          )}
+        </ul>
+            <ul>
+              {["a", "b", "c", ""].map((c,i) => (
+                <li className="bg-red-500 p-2 empty:bg-blue-500" key={i}>
+                    {c}
+                  </li> ))}
+              </ul>
         <div className=" flex justify-between mt-2 pt-2 border-t-2 border-dashed">
           <span className=" ">Total</span>
           <span className=" font-semibold">$19</span>
         </div>
-        <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/4 mx-auto">
-          <span className=" ">Checkout</span>
-          
-        </div>                
+        <div className="flex items-center">
+          <button className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-3/4 mx-auto hover:bg-teal-500 hover:text-black
+          active:bg-yellow-500 focus:bg-red-500
+          ">
+            Checkout
+          </button>  
+        </div>              
       </div>
-      <div className=" bg-white overflow-hidden rounded-3xl shadow-xl">
-        <div className="bg-blue-500 p-6 pb-14">
+      <div className=" bg-white overflow-hidden rounded-3xl shadow-xl group ">
+        <div className="bg-blue-500 p-6 pb-14 group-hover:bg-orange-200">
           <span className=" text-white text-2xl">Profile</span>
         </div>
         <div className=" rounded-3xl p-6 bg-white relative -top-5">
@@ -32,7 +42,7 @@ const Home: NextPage = () => {
               <span className=" text-sm text-gray-500">Orders</span>
               <span className="font-medium">340</span>
             </div>
-            <div className="h-24 w-24 bg-red-400 rounded-full" />
+            <div className="h-24 w-24 bg-zinc-400 rounded-full group-hover:bg-red-200" />
             <div className=" flex flex-col items-center">
               <span className=" text-sm text-gray-500">Spent</span>
               <span className="font-medium">$2,310</span>
@@ -59,9 +69,9 @@ const Home: NextPage = () => {
             <span className="text-xs text-gray-500">Chair</span>
             <div className="mt-3 mb-5 flex justify-between items-center">
               <div className="space-x-2">
-                <button className="w-5 h-5 rounded-full bg-yellow-500"></button>
-                <button className="w-5 h-5 rounded-full bg-indigo-500"></button>
-                <button className="w-5 h-5 rounded-full bg-teal-500"></button>
+                <button className="w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500 transition"></button>
+                <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 transition"></button>
+                <button className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2 ring-teal-500 transition"></button>
               </div>
               <div className="flex justify-between items-center space-x-5">
                 <button className=" p-1.5 rounded-lg bg-blue-200 flex justify-between items-center aspect-square w-8 text-xl text-gray-500">-</button>
