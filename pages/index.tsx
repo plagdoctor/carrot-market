446c26d7b706548fbe2ab2e20e251e06964a28c6
jinyoung <danroom85@gmail.com>
2022-01-22 14:34:52 +0900

    tailwind list conotrol+marker

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,18 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <form className="flex flex-col space-y-2 p-5 focus-within:bg-blue-100">
-      <input type="text"
-      required
-      placeholder="UserName"
-      className=" border p-1 peer border-gray-400 rounded-full placeholder:text-red-500"
-      />
-      <span className=" hidden peer-invalid:block peer-invalid:text-red-500">This input is invalid</span>
-      <span className=" hidden peer-valid:block peer-invalid:text-teal-500">awesome Username</span>
-      <span className=" hidden peer-hover:block peer-invalid: text-orange-400">hello</span>
-      
-      <input type="submit" value="Login" className="bg-white"/>
-    </form>
+    <div className="flex flex-col space-y-2 p-5 bg-red-400">
+      <details className="select-none open:text-white open:bg-indigo-400">
+        <summary className="cursor-pointer">What is my fav. food.</summary>
+        <span className="">피자</span>
+        <ul className=" list-decimal marker:text-teal-500">
+          <li>hi</li>
+          <li>hi</li>
+          <li>hi</li>
+        </ul>
+        
+      </details>
+    </div>
   );
 };
 
