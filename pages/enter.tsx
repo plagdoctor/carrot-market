5dccde27117c8d61dc25f5e612fe26510c063361
jinyoung <danroom85@gmail.com>
2022-01-31 17:29:18 +0900

    finish items page

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,48 +1,53 @@
 import { useState } from "react";
 
+function cls(...classnames:string[]) {
+    return classnames.join(" ");
+
+}
+
 export default function Enter() {
   const [method, setMethod] = useState<"email" | "phone">("email");
   const onEmailClick = () => setMethod("email");
   const onPhoneClick = () => setMethod("phone");
   return (
-    <div className=" mt-16">
+    <div className=" mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
       <div className=" mt-8">
         <div className=" flex flex-col items-center">
           <h5 className=" text-sm text-gray-500 font-medium  ">Enter using:</h5>
           <div className=" mt-8 border-b w-full grid grid-cols-2 gap-16 ">
-            <button className={` pb-4 font-medium ${ method === "email" ? "border-b-2 text-orange-400  border-orange-400" : " " }`} onClick={onEmailClick}>Email address</button>
-            <button className={` pb-4 font-medium ${ method === "phone" ? "border-b-2 text-orange-400  border-orange-400" : " " }`} onClick={onPhoneClick}>Phone number</button>
+            <button className={ cls("pb-4 font-medium border-b-2", method === "email" ? " text-orange-400  border-orange-400" : " border-transparent text-gray-500" ) } onClick={onEmailClick}>Email address</button>
+            <button className={ cls("pb-4 font-medium border-b-2", method === "phone" ? " text-orange-400  border-orange-400" : " border-transparent text-gray-500" ) } onClick={onPhoneClick}>Phone number</button>
           </div>
         </div>
-        <form>
-          <label>
+        <form className="flex flex-col mt-8">
+          <label className="text-sm font-medium text-gray-700 ">
             {method === "email" ? "Email address" : null}
             {method === "phone" ? "Phone number" : null}
           </label>
-          <div>
-            {method === "email" ? <input type="email" required /> : null}
+          <div className="mt-1">
+            {method === "email" ? <input type="email" className=" rounded-xl shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500 appearance-none w-full px-3 py-2 border border-gray-300" required /> : null}
             {method === "phone" ? (
-              <div>
-                <span>+82</span>
-                <input type="number" required />
+              <div className="flex rounded-sm shadow-sm">
+                <span className="flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm select-none">
+                    +82
+                </span>
+                <input type="number" className=" rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500 appearance-none w-full px-3 py-2 border border-gray-300" required />
               </div>
             ) : null}
           </div>
-          <button>
+          <button className=" mt-6 bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-lg shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
             {method === "email" ? "Get login link" : null}
             {method === "phone" ? "Get one-time password" : null}
           </button>
         </form>
-        <div>
-          <div>
-            <div />
-            <div>
-              <span>Or enter with</span>
+        <div className="mt-8">
+          <div className=" absolute w-full border-t border-gray-300" />
+            <div className="relative -top-3 text-center ">
+              <span className="bg-white px-2 text-sm text-gray-500  ">Or enter with</span>
             </div>
-          </div>
-          <div>
-            <button>
+          <div className=" grid grid-cols-2 gap-3 mt-2 ">
+            <button className="flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
               <svg
                 className="w-5 h-5"
                 aria-hidden="true"
@@ -52,7 +57,7 @@ export default function Enter() {
                 <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
               </svg>
             </button>
-            <button>
+            <button  className="flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
               <svg
                 className="w-5 h-5"
                 aria-hidden="true"
--- pages/index_bak2.tsx
+++ pages/index_bak2.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col space-y-2 p-5">
-      <p className="first-letter:text-7xl">hellooo everybody</p>
+      <p className="first-letter:text-7xl first-line:">hellooo everybody</p>
     </div>
   );
 };
