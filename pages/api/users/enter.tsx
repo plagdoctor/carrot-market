08a1c2bbabbab36892934679c31e84ac6cf8e95f
jinyoung <danroom85@gmail.com>
2022-02-20 10:03:51 +0900

    change setting

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -7,9 +7,11 @@ async function handler(
     req:NextApiRequest, res:NextApiResponse
 ){
     const { phone, email} = req.body;
-    const payload = phone ? {phone: +phone} : {email};
+    const user = phone ? {phone: +phone} : {email};
+    const payload = Math.floor(100000 + Math.random() * 900000) + "";
 
-    const user = await client.user.upsert({
+    //3단계 : payload 를 만들어 중복적인 구문 제거 
+/*     const user = await client.user.upsert({
         where: {
             ...payload,
             // ...( phone ? { phone: +phone} : {}),
@@ -23,34 +25,37 @@ async function handler(
         },
         update: {},
 
-    });    
-    // if(phone){
-    //     user = await client.user.upsert({
-    //         where: {
-    //             phone: +phone,
-    //         },
-    //         create: {
-    //             name:"Anonymous",
-    //             phone: +phone,
-    //         },
-    //         update: {},
+    });     */
 
-    //     });
-    // }else if(email){
-    //     user = await client.user.upsert({
-    //         where: {
-    //             email,
-    //         },
-    //         create: {
-    //             name:"Anonymous",
-    //             email,
-    //         },
-    //         update: {},
-    
-    //     });
-    // }
+    //2단계 : upsert 로 인서트와 가져오기를 같이 함
+/*     if(phone){
+        user = await client.user.upsert({
+            where: {
+                phone: +phone,
+            },
+            create: {
+                name:"Anonymous",
+                phone: +phone,
+            },
+            update: {},
 
+        });
+    }else if(email){
+        user = await client.user.upsert({
+            where: {
+                email,
+            },
+            create: {
+                name:"Anonymous",
+                email,
+            },
+            update: {},
     
+        });
+    } */
+
+
+// 1단계 이메일이나 폰넘버를 각각 findUnique 해서 생성 또는 가져옴
 /*     if(email){
         user = await client.user.findUnique({
             where: {
@@ -86,7 +91,29 @@ async function handler(
         }
     }    */ 
     
-    console.log(user);
+    const token = await client.token.create({
+        data: {
+            payload,
+            user: {
+                connectOrCreate: {
+                    where: {
+                        ...user,
+                        // ...( phone ? { phone: +phone} : {}),
+                        // ...( email ? { email} : {}),
+                    },
+                    create: {
+                        name:"Anonymous",
+                        ...user,
+                        // ...( phone ? { phone: +phone} : {}),
+                        // ...( email ? { email} : {}),
+                    },
+            
+                },
+            }
+        }
+    })
+
+    console.log(token);
     return res.status(200).end();
 }
 
