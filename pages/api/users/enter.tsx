5e3544d5588cb8c2bccffcfa06f5be60e27c1c1e
jinyoung <danroom85@gmail.com>
2022-02-24 23:31:09 +0900

    reverted

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,13 +1,17 @@
+
+import twilio from "twilio";
 import client from "@libs/server/client";
-import withHandler from "@libs/server/withHandler";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
-    req:NextApiRequest, res:NextApiResponse
+    req:NextApiRequest, res:NextApiResponse<ResponseType>
 ){
     const { phone, email} = req.body;
-    const user = phone ? {phone: +phone} : {email};
+    const user = phone ? {phone: +phone} : email ? {email} : null;
+    if(!user) return res.status(400).json({ ok: false});
     const payload = Math.floor(100000 + Math.random() * 900000) + "";
 
     //3단계 : payload 를 만들어 중복적인 구문 제거 
@@ -112,9 +116,19 @@ async function handler(
             }
         }
     })
+    if (phone) {
+        const message = await twilioClient.messages.create({
+            messagingServiceSid: process.env.TWILIO_MS_SID,
+            to: process.env.PHONE_NO!,
+            body: `Your login token is ${payload}.`,
+    
+        });
+        console.log(message);   
+    } 
 
-    console.log(token);
-    return res.status(200).end();
+    return res.json({
+        ok:true,
+    });
 }
 
 export default withHandler("POST", handler);
