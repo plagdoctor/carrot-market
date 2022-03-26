dfa3a7805e6668f1bf15b39213678112fb068af6
jinyoung <danroom85@gmail.com>
2022-03-27 07:49:38 +0900

    image upload

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -4,13 +4,14 @@ import Input from "@components/input";
 import Layout from "@components/layout";
 import useUser from "@libs/client/useUser";
 import { useForm } from "react-hook-form";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useMutation from "@libs/client/useMutation";
 
 interface EditProfileForm {
   email?: string;
   phone?: string;
   name?: string;
+  avatar?: FileList;
   formErrors?: string;
 }
 
@@ -21,14 +22,15 @@ interface EditProfileResponse {
 
 const EditProfile: NextPage = () => {
   const {user} = useUser();
-  const {register, setValue, handleSubmit, setError, formState:{errors}} = useForm<EditProfileForm>();
+  const {register, setValue, handleSubmit, setError, formState:{errors},watch} = useForm<EditProfileForm>();
   useEffect(() => {
     if(user?.email) setValue("email", user.email);
     if(user?.phone) setValue("phone", user.phone);
     if(user?.name) setValue("name", user.name);
   },[user, setValue]);
   const [editProfile,{data, loading}] = useMutation<EditProfileResponse>(`/api/users/me`);
-  const onValid = ({phone, email, name}: EditProfileForm) => {
+  const onValid = ({phone, email, name, avatar}: EditProfileForm) => {
+
     //console.log(form);
     if(loading) return;
     if (email === '' && phone ==='' && name ===''){
@@ -38,24 +40,41 @@ const EditProfile: NextPage = () => {
       editProfile({email: email !== user?.email ? email : "" ,
                  phone: phone !== user?.phone ? phone : ""
                 }); */
-    editProfile({email, phone, name});                
+    editProfile({email, phone, name, avatar});                
   }
   useEffect(() => {
     if(data && !data.ok && data.error){
       setError("formErrors", {message:data.error});
     }
-  },[data])
+  },[data, setError]);
+  const [avatarPreview, setAvatarPreview] = useState("");
+
+  const avatar = watch("avatar");
+  useEffect(() => {
+    if(avatar && avatar.length > 0){
+      const file = avatar[0]
+      setAvatarPreview(URL.createObjectURL(file));
+    }
+  },[avatar])
   return (
     <Layout canGoBack title="Edit Profile">
       <form onSubmit={handleSubmit(onValid)} className="py-10 px-4 space-y-4">
         <div className="flex items-center space-x-3">
-          <div className="w-14 h-14 rounded-full bg-slate-500" />
+          {avatarPreview ? (
+            <img
+              src={avatarPreview}
+              className="w-14 h-14 rounded-full bg-slate-500"
+            />
+          ) : (
+            <div className="w-14 h-14 rounded-full bg-slate-500" />
+          )}
           <label
             htmlFor="picture"
             className="cursor-pointer py-2 px-3 border hover:bg-gray-50 border-gray-300 rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 text-gray-700"
           >
             Change
             <input
+              {...register("avatar")}
               id="picture"
               type="file"
               className="hidden"
